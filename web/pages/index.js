9c6dbd079bc8377e5586d76be8541d97b29189b0
Jeremy Davis <jeremyadamdavis@gmail.com>
2019-03-13 11:26:42 -0400

    Added ApolloClient and build simple components to test

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,31 +1,43 @@
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import ExchangeRates from '../components/ExchangeRates';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io'
+});
+
 function Index(props) {
   return (
-    <div>
-      Click{' '}
-      <Link href="/about">
-        <a>here</a>
-      </Link>{' '}
-      to read more
-      <Link as={`/p/HowTo`} href="/post?title='How to do this thing'">
-        <a>POST</a>
-      </Link>
-      <hr />
+    <ApolloProvider client={client}>
       <div>
-        <h1>Batman TV Shows</h1>
-        <ul>
-          {props.shows.map(({ show }) => (
-            <li key={show.id}>
-              <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-                <a>{show.name}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        Click{' '}
+        <Link href="/about">
+          <a>here</a>
+        </Link>{' '}
+        to read more
+        <Link as={`/p/HowTo`} href="/post?title='How to do this thing'">
+          <a>POST</a>
+        </Link>
+        <hr />
+        <div>
+          <h1>Batman TV Shows</h1>
+          <ul>
+            {props.shows.map(({ show }) => (
+              <li key={show.id}>
+                <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                  <a>{show.name}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ExchangeRates />
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
