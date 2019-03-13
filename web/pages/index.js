4a55a0f46583e2276193c475eb8f43b40be72efb
Jeremy Davis <jeremyadamdavis@gmail.com>
2019-03-13 14:32:51 -0400

    switched to using Zeits apollo client boilerplate

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,38 +1,14 @@
-import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
-
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
+import App from '../components/App';
+import Header from '../components/Header';
 import SchoolsList from '../components/SchoolsList';
-
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8080/v1alpha1/graphql',
-  headers: {
-    'x-hasura-admin-secret': publicRuntimeConfig.HASURA_GRAPHQL_ADMIN_SECRET
-  }
-});
 
 function Index(props) {
   return (
-    <ApolloProvider client={client}>
+    <App>
+      <Header />
       <SchoolsList />
-    </ApolloProvider>
+    </App>
   );
 }
-
-// Index.getInitialProps = async function() {
-//   // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-//   // const data = await res.json();
-
-//   // console.log(`Show data fetched. Count: ${data.length}`);
-
-//   return {
-//     // shows: data
-//   };
-// };
 
 export default Index;
